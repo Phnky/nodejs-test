87d03579ded1bc9997add5aeba390e508a993d5f
gitPhunky <jabayquen@gmail.com>
2018-04-22 00:34:23 -0700

    update commits

--- test.js
+++ test.js
@@ -3,7 +3,14 @@ var dt = require("./datetime.js");
 var url = require("url");
 // require function includes a module(similar to js libraries)
 // ./ is needed to find a module in the same folder as test.js
-// The url module splits the query string into readable parts.
+// The url module can split the query string into readable parts.
+var fs = require("fs");
+// fs allows the ability to work with the file system on the machine:
+// read files
+// write files
+// update files
+// delete files
+// rename files
 
 // the built in http module can be used to create an HTTP server that listens to server ports and gives a response back to the client. The createServer() method is used to create the server.
 http.createServer(function(req, res) {
