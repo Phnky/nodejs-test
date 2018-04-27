c26b866c0445c0bd535d165d6aaa098fd39d5b39
gitPhunky <jabayquen@gmail.com>
2018-04-26 19:47:11 -0700

    response window

--- client-script.js
+++ client-script.js
@@ -2,5 +2,7 @@
 //event handlers
 document.getElementById("open-file").onclick = function(){
   console.log("open-file clicked.");
-  window.open("http://localhost:8080");
+  //window.open("http://localhost:8080");
+  res = "http://localhost:8080";
+  document.getElementById("response-window").innerHTML = '<object type="text/html" data="http://localhost:8080">object</object>';
 };
